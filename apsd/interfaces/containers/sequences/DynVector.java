fc9f6fc05b8612213bb23c66d0ee2d656a271fb5
Gabbinetto <gabrieleferrante97@googlemail.com>
2025-12-03 16:58:10 +0100

    Implemented Vector and DynVector (Without ShiftLeft and ShiftRight)

--- apsd/interfaces/containers/sequences/DynVector.java
+++ apsd/interfaces/containers/sequences/DynVector.java
@@ -1,32 +1,69 @@
 package apsd.interfaces.containers.sequences;
 
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.ResizableContainer;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.ResizableContainer;
 
-public interface DynVector<Data> { // Must extend ResizableContainer, InsertableAtSequence, RemovableAtSequence, and Vector
-
-  /* ************************************************************************ */
-  /* Override specific member functions from InsertableAtSequence             */
-  /* ************************************************************************ */
-
-  // ...
-
-  /* ************************************************************************ */
-  /* Override specific member functions from RemovableAtSequence              */
-  /* ************************************************************************ */
-
-  // ...
+public interface DynVector<Data>
+    extends Vector<Data>, ResizableContainer, InsertableAtSequence<Data>, RemovableAtSequence<Data> { // Must
+                                                                                                      // extend
+                                                                                                      // ResizableContainer,
+                                                                                                      // InsertableAtSequence,
+                                                                                                      // RemovableAtSequence,
+                                                                                                      // and
+                                                                                                      // Vector
 
   /* ************************************************************************ */
-  /* Specific member functions of Vector                                       */
+  /* Override specific member functions from InsertableAtSequence */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default void InsertAt(Data dat, Natural num) {
+    long idx = ExcIfOutOfBound(num);
+    Natural size = Size();
+
+    if (size.compareTo(Capacity()) >= 0) {
+      Expand();
+    }
+
+    SetAt(dat, size);
+
+    ShiftLeft(size, Natural.Of(size.ToLong() - idx));
+  }
 
   /* ************************************************************************ */
-  /* Override specific member functions from Container                        */
+  /* Override specific member functions from RemovableAtSequence */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default Data AtNRemove(Natural num) {
+    long idx = ExcIfOutOfBound(num);
+    Data dat = GetAt(num);
+    Natural size = Size();
+
+    ShiftRight(num, Natural.Of(size.ToLong() - 1L - idx));
+
+    SetAt(null, size.Decrement());
+
+    if (size.Decrement().ToLong() < Capacity().ToLong() / THRESHOLD_FACTOR)
+      Shrink();
+
+    return dat;
+  }
+
+  /* ************************************************************************ */
+  /* Specific member functions of Vector */
+  /* ************************************************************************ */
+
+  @Override
+  default DynVector<Data> SubVector(Natural start, Natural end) {
+    return (DynVector<Data>) Vector.super.SubVector(start, end);
+  }
+
+  /* ************************************************************************ */
+  /* Override specific member functions from Container */
+  /* ************************************************************************ */
+
+  @Override
+  Natural Size();
 
 }
--- apsd/interfaces/containers/sequences/Vector.java
+++ apsd/interfaces/containers/sequences/Vector.java
@@ -1,47 +1,91 @@
 package apsd.interfaces.containers.sequences;
 
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.ReallocableContainer;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.ReallocableContainer;
 
-public interface Vector<Data> { // Must extend ReallocableContainer and MutableSequence
+public interface Vector<Data> extends ReallocableContainer, MutableSequence<Data> { // Must extend ReallocableContainer
+                                                                                    // and MutableSequence
 
   // ShiftLeft
+  default void ShiftLeft(Natural pos) {
+    ShiftLeft(pos, Natural.ONE);
+  }
 
-  // default void ShiftLeft(Natural pos, Natural num) {
-  //   long idx = ExcIfOutOfBound(pos);
-  //   long size = Size().ToLong();
-  //   long len = num.ToLong();
-  //   len = (len <= size - idx) ? len : size - idx;
-  //   if (len > 0) {
-  //     long iniwrt = idx;
-  //     long wrt = iniwrt;
-  //     for (long rdr = wrt + len; rdr < size; rdr++, wrt++) {
-  //       Natural natrdr = Natural.Of(rdr);
-  //       SetAt(GetAt(natrdr), Natural.Of(wrt));
-  //       SetAt(null, natrdr);
-  //     }
-  //     for (; wrt - iniwrt < len; wrt++) {
-  //       SetAt(null, Natural.Of(wrt));
-  //     }
-  //   }
-  // }
+  default void ShiftLeft(Natural pos, Natural num) {
+    long idx = ExcIfOutOfBound(pos); // 4
+    long size = Size().ToLong(); // 10
+    long len = num.ToLong(); // 7
+    len = (len <= size - idx) ? len : size - idx;
+    if (len > 0) {
+      long iniwrt = idx;
+      long wrt = iniwrt;
+      for (long rdr = wrt + len; rdr < size; rdr++, wrt++) {
+        Natural natrdr = Natural.Of(rdr);
+        SetAt(GetAt(natrdr), Natural.Of(wrt));
+        SetAt(null, natrdr);
+      }
+      for (; wrt - iniwrt < len; wrt++) {
+        SetAt(null, Natural.Of(wrt));
+      }
+    }
+  }
 
   // ShiftFirstLeft
+  default void ShiftFirstLeft() {
+    ShiftLeft(Natural.ZERO);
+  }
 
   // ShiftLastLeft
+  default void ShiftLastLeft() {
+    ShiftLeft(Size().Decrement());
+  }
 
   // ShiftRight
+  default void ShiftRight(Natural pos) {
+    ShiftRight(pos, Natural.ONE);
+  }
+
+  default void ShiftRight(Natural pos, Natural num) {
+    long idx = ExcIfOutOfBound(pos);
+    long size = Size().ToLong();
+    long len = num.ToLong();
+    len = (len <= size - idx) ? len : size - idx;
+    if (len > 0) {
+      long iniwrt = idx;
+      long wrt = iniwrt;
+      for (long rdr = wrt - len; rdr >= idx; rdr--, wrt--) {
+        Natural natrdr = Natural.Of(rdr);
+        SetAt(GetAt(natrdr), Natural.Of(wrt));
+        SetAt(null, natrdr);
+      }
+      for (; wrt > idx; wrt--) {
+        SetAt(null, Natural.Of(wrt));
+      }
+    }
+  }
 
   // ShiftFirstRight
+  default void ShiftFirstRight() {
+    ShiftRight(Natural.ZERO);
+  }
 
   // ShiftLastRight
+  default void ShiftLastRight() {
+    ShiftRight(Size().Decrement());
+  }
 
   // SubVector
+  default Vector<Data> SubVector(Natural start, Natural end) {
+    return (Vector<Data>) SubSequence(start, end);
+  }
 
   /* ************************************************************************ */
-  /* Override specific member functions from Container                        */
+  /* Override specific member functions from Container */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default Natural Size() {
+    return Capacity();
+  }
 
 }
