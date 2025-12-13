87c057363a3a03e8584187c4a56c6b27a43b6008
Gabbinetto <gabrieleferrante97@googlemail.com>
2025-12-13 16:58:23 +0100

    Partial containers/deqs implementation

--- apsd/classes/containers/deqs/WQueue.java
+++ apsd/classes/containers/deqs/WQueue.java
@@ -1,40 +1,74 @@
 package apsd.classes.containers.deqs;
 
-// import apsd.classes.containers.collections.concretecollections.VList;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.List;
-// import apsd.interfaces.containers.deqs.Queue;
+import apsd.classes.containers.collections.concretecollections.VList;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.List;
+import apsd.interfaces.containers.deqs.Queue;
 
 /** Object: Wrapper queue implementation. */
-public class WQueue<Data> { // Must implement Queue
+public class WQueue<Data> implements Queue<Data> { // Must implement Queue
 
-  // protected final List<Data> lst;
+  protected final List<Data> lst;
 
-  // public WQueue()
+  public WQueue() {
+    this.lst = new VList<Data>();
+  }
 
-  // public WQueue(List<Data> lst)
+  public WQueue(List<Data> lst) {
+    this.lst = lst;
+  }
 
-  // public WQueue(TraversableContainer<Data> con)
+  public WQueue(TraversableContainer<Data> con) {
+    this.lst = new VList<Data>(con);
+  }
 
-  // public WQueue(List<Data> lst, TraversableContainer<Data> con)
+  public WQueue(List<Data> lst, TraversableContainer<Data> con) {
+    this.lst = lst;
+    con.TraverseForward(
+        (dat) -> {
+          this.lst.Insert(dat);
+          return false;
+        });
+  }
 
   /* ************************************************************************ */
-  /* Override specific member functions from Container                        */
+  /* Override specific member functions from Container */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Natural Size() {
+    return this.lst.Size();
+  }
 
   /* ************************************************************************ */
-  /* Override specific member functions from ClearableContainer               */
+  /* Override specific member functions from ClearableContainer */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void Clear() {
+    this.lst.Clear();
+  }
 
   /* ************************************************************************ */
-  /* Override specific member functions from Queue                            */
+  /* Override specific member functions from Queue */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void Enqueue(Data dat) {
+    this.lst.InsertLast(dat);
+  }
+
+  @Override
+  public void Dequeue() {
+    this.lst.RemoveFirst();
+  }
+
+  @Override
+  public Data Head() {
+    return this.lst.GetFirst();
+  }
+
+  // TODO: Reimplementare HeadNDequeue in qualche modo
 
 }
--- apsd/classes/containers/deqs/WStack.java
+++ apsd/classes/containers/deqs/WStack.java
@@ -1,40 +1,74 @@
 package apsd.classes.containers.deqs;
 
-// import apsd.classes.containers.collections.concretecollections.VList;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.List;
-// import apsd.interfaces.containers.deqs.Stack;
+import apsd.classes.containers.collections.concretecollections.VList;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.List;
+import apsd.interfaces.containers.deqs.Stack;
 
 /** Object: Wrapper stack implementation. */
-public class WStack<Data> { // Must implement Stack
+public class WStack<Data> implements Stack<Data> { // Must implement Stack
 
-  // protected final List<Data> lst;
+  protected final List<Data> lst;
 
-  // public WStack()
+  public WStack() {
+    this.lst = new VList<Data>();
+  }
 
-  // public WStack(List<Data> lst)
+  public WStack(List<Data> lst) {
+    this.lst = lst;
+  }
 
-  // public WStack(TraversableContainer<Data> con)
+  public WStack(TraversableContainer<Data> con) {
+    this.lst = new VList<Data>(con);
+  }
 
-  // public WStack(List<Data> lst, TraversableContainer<Data> con)
+  public WStack(List<Data> lst, TraversableContainer<Data> con) {
+    this.lst = lst;
+    con.TraverseForward(
+        (dat) -> {
+          this.lst.Insert(dat);
+          return false;
+        });
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Container                        */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Natural Size() {
+    return this.lst.Size();
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from ClearableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void Clear() {
+    this.lst.Clear();
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Stack                            */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void Push(Data dat) {
+    this.lst.InsertLast(dat);
+  }
+
+  @Override
+  public void Pop() {
+    this.lst.RemoveLast();
+  }
+  
+  @Override
+  public Data Top() {
+    return this.lst.GetLast();
+  }
+
+  // TODO: Reimplementare in qualche modo TopNPop, TopNSwap e SwapTop
 
 }
